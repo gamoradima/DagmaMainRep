01c93be2167d61bb7bac55511eb7ded77000bb01
gamoradima <gamoradima@gmail.com>
2024-01-10 15:01:45 +0200

    Contacts and Realty FreedomUI customization

---
+++ UsrApp_6z5e6aa/Schemas/Contacts_FormPage/Contacts_FormPage.js
@@ -0,0 +1,44 @@
+define("Contacts_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "UsrExternalId",
+				"values": {
+					"layoutConfig": {
+						"column": 2,
+						"row": 4,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.PDS_UsrExternalId_6tz5nql",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrExternalId_6tz5nql"
+				},
+				"parentName": "ContactInfoFieldsContainer",
+				"propertyName": "items",
+				"index": 6
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"PDS_UsrExternalId_6tz5nql": {
+						"modelConfig": {
+							"path": "PDS.UsrExternalId"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
